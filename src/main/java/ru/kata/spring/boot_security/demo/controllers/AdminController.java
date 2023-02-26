54853457f586fbab7d5ee7789ab598d1adf4536e
Ap4u11 <artyom-panfilov@yandex.ru>
2023-02-27 00:15:00 +0300

    com

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,52 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.entities.User;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final UserService userService;
+    public AdminController(UserService userService){
+        this.userService=userService;
+    }
+    @GetMapping
+    public String showAllUsers(Model model) {
+        model.addAttribute("users", userService.showUsers());
+        return "admin/admin";
+    }
+
+    @GetMapping ("/create")
+    public String addNewUser(Model model) {
+        User user = new User();
+        model.addAttribute("user",user);
+        model.addAttribute("listRoles",userService.listRoles());
+        return "admin/create";
+    }
+    @PostMapping("/create")
+    public String saveUser(@ModelAttribute("user") User user){
+        userService.save(user);
+        return "redirect:/admin";
+    }
+    @GetMapping("/{id}/update")
+    public String updateUser(@PathVariable("id") Long id, Model model){
+        model.addAttribute("user", userService.getUser(id));
+        model.addAttribute("listRoles",userService.listRoles());
+        return "admin/update";
+    }
+    @PatchMapping("/{id}")
+    public String saveUpdateUser(@ModelAttribute("user") User user){
+        userService.update(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public String deleteUser(@PathVariable(name = "id") Long id ){
+        userService.delete(id);
+        return "redirect:/admin";
+    }
+
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/entities/Role.java
@@ -0,0 +1,63 @@
+package ru.kata.spring.boot_security.demo.entities;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Override
+    public String getAuthority() {
+        return getRoleName();
+    }
+    public Role() {
+}
+    public Role(String roleName){
+        this.roleName=roleName;
+}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return this.roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime =31;
+        int result =1;
+        result =prime*result+((id==null)?0:id.hashCode());
+        return result;
+    }
+}
